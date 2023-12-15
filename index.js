c137c6614ab60f0e15237d1141bf74abe95efeb1
Htet Aung Khant <htetaung23.ha@gmail.com>
2023-12-15 15:37:00 +0630

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
+import swaggerUI from "swagger-ui-express";
+import express from "express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 const data = [
   {
@@ -61,37 +61,45 @@ const data = [
 ];
 
 const url = "https://express-app-with-swagger.vercel.app";
+// const url = "http://localhost:2000";
 
 const app = express();
 app.use(express.json());
 
 const options = {
   definition: {
-    openapi: "3.1.0",
+    openapi: "3.0.0",
     info: {
-      title: "Dota2 Heros API",
-      version: "0.1.0",
-      description: "Dota 2 heros api by Florject",
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
     },
     servers: [
       {
         url,
-        // url: "http://localhost:3000",
+        description: "My API Documentation",
       },
     ],
   },
-  apis: ["src/**/*.js"],
+  // This is to call all the file
+  apis: ["./*.js"],
 };
 
-const specs = swaggerJsdoc(options);
+const specs = swaggerJsDoc(options);
 
 const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css";
 
 app.use(
   "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(specs, { customCssUrl: CSS_URL })
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { customCssUrl: CSS_URL })
 );
 
 app.get("/", (req, res, next) => {
@@ -101,13 +109,28 @@ app.get("/", (req, res, next) => {
   res.redirect(`${url}/api-docs`);
 });
 
+/**
+ * @swagger
+ * /api/heros:
+ *   get:
+ *     summary: Lists all the heros
+ *     tags: [Heros]
+ *     responses:
+ *       200:
+ *         description: The list of the heros
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Heros'
+ */
+
 app.get("/api/heros", (req, res, next) => {
   console.log("work");
   res.send(data);
 });
 
-app.listen(3000, () => {
+app.listen(2000, () => {
   console.log("server started");
 });
-
-module.exports = app;
