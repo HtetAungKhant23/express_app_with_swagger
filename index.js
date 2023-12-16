f5ec023ef08e6e0dc58d524c6b4a3146bea34064
Htet Aung Khant <htetaung23.ha@gmail.com>
2023-12-16 09:26:10 +0630

    fix

--- index.js
+++ index.js
@@ -1,67 +1,10 @@
 import swaggerUI from "swagger-ui-express";
 import express from "express";
 import swaggerJsDoc from "swagger-jsdoc";
+import docRouter from "./routes/doc.js";
 
-const data = [
-  {
-    id: 1,
-    name: "npc_dota_hero_antimage",
-    localized_name: "Anti-Mage",
-    primary_attr: "agi",
-    attack_type: "Melee",
-    roles: ["Carry", "Escape", "Nuker"],
-    legs: 2,
-    imageUrl:
-      "https://liquipedia.net//commons/images/thumb/7/70/Anti-Mage_Large.png/125px-Anti-Mage_Large.png",
-  },
-  {
-    id: 2,
-    name: "npc_dota_hero_axe",
-    localized_name: "Axe",
-    primary_attr: "str",
-    attack_type: "Melee",
-    roles: ["Initiator", "Durable", "Disabler", "Carry"],
-    legs: 2,
-    imageUrl:
-      "https://liquipedia.net//commons/images/thumb/a/a1/Axe_Large.png/125px-Axe_Large.png",
-  },
-  {
-    id: 3,
-    name: "npc_dota_hero_bane",
-    localized_name: "Bane",
-    primary_attr: "all",
-    attack_type: "Ranged",
-    roles: ["Support", "Disabler", "Nuker", "Durable"],
-    legs: 4,
-    imageUrl:
-      "https://liquipedia.net//commons/images/thumb/7/72/Bane_Large.png/125px-Bane_Large.png",
-  },
-  {
-    id: 4,
-    name: "npc_dota_hero_bloodseeker",
-    localized_name: "Bloodseeker",
-    primary_attr: "agi",
-    attack_type: "Melee",
-    roles: ["Carry", "Disabler", "Nuker", "Initiator"],
-    legs: 2,
-    imageUrl:
-      "https://liquipedia.net//commons/images/thumb/a/a6/Bloodseeker_Large.png/125px-Bloodseeker_Large.png",
-  },
-  {
-    id: 5,
-    name: "npc_dota_hero_crystal_maiden",
-    localized_name: "Crystal Maiden",
-    primary_attr: "int",
-    attack_type: "Ranged",
-    roles: ["Support", "Disabler", "Nuker"],
-    legs: 2,
-    imageUrl:
-      "https://liquipedia.net//commons/images/thumb/d/dc/Crystal_Maiden_Large.png/125px-Crystal_Maiden_Large.png",
-  },
-];
-
-// const url = "https://express-app-with-swagger.vercel.app";
-const url = "http://localhost:2000";
+const url = "https://express-app-with-swagger.vercel.app";
+// const url = "http://localhost:2000";
 
 const app = express();
 app.use(express.json());
@@ -88,7 +31,7 @@ const options = {
     ],
   },
   // This is to call all the file
-  apis: ["src/routes/*.js"],
+  apis: ["routes/*.js"],
 };
 
 const specs = swaggerJsDoc(options);
@@ -109,27 +52,12 @@ app.get("/", (req, res, next) => {
   res.redirect(`${url}/api-docs`);
 });
 
-/**
- * @swagger
- * /api/heros:
- *   get:
- *     summary: Lists all the heros
- *     tags: [Heros]
- *     responses:
- *       200:
- *         description: The list of the heros
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Heros'
- */
+app.use("/api/heros", docRouter);
 
-app.get("/api/heros", (req, res, next) => {
-  console.log("work");
-  res.send(data);
-});
+// app.get("/api/heros", (req, res, next) => {
+//   console.log("work");
+//   res.send(data);
+// });
 
 app.listen(2000, () => {
   console.log("server started");
--- routes/doc.js
+++ routes/doc.js
@@ -1,3 +1,65 @@
+import express from "express";
+
+const docRouter = express.Router();
+
+const data = [
+  {
+    id: 1,
+    name: "npc_dota_hero_antimage",
+    localized_name: "Anti-Mage",
+    primary_attr: "agi",
+    attack_type: "Melee",
+    roles: ["Carry", "Escape", "Nuker"],
+    legs: 2,
+    imageUrl:
+      "https://liquipedia.net//commons/images/thumb/7/70/Anti-Mage_Large.png/125px-Anti-Mage_Large.png",
+  },
+  {
+    id: 2,
+    name: "npc_dota_hero_axe",
+    localized_name: "Axe",
+    primary_attr: "str",
+    attack_type: "Melee",
+    roles: ["Initiator", "Durable", "Disabler", "Carry"],
+    legs: 2,
+    imageUrl:
+      "https://liquipedia.net//commons/images/thumb/a/a1/Axe_Large.png/125px-Axe_Large.png",
+  },
+  {
+    id: 3,
+    name: "npc_dota_hero_bane",
+    localized_name: "Bane",
+    primary_attr: "all",
+    attack_type: "Ranged",
+    roles: ["Support", "Disabler", "Nuker", "Durable"],
+    legs: 4,
+    imageUrl:
+      "https://liquipedia.net//commons/images/thumb/7/72/Bane_Large.png/125px-Bane_Large.png",
+  },
+  {
+    id: 4,
+    name: "npc_dota_hero_bloodseeker",
+    localized_name: "Bloodseeker",
+    primary_attr: "agi",
+    attack_type: "Melee",
+    roles: ["Carry", "Disabler", "Nuker", "Initiator"],
+    legs: 2,
+    imageUrl:
+      "https://liquipedia.net//commons/images/thumb/a/a6/Bloodseeker_Large.png/125px-Bloodseeker_Large.png",
+  },
+  {
+    id: 5,
+    name: "npc_dota_hero_crystal_maiden",
+    localized_name: "Crystal Maiden",
+    primary_attr: "int",
+    attack_type: "Ranged",
+    roles: ["Support", "Disabler", "Nuker"],
+    legs: 2,
+    imageUrl:
+      "https://liquipedia.net//commons/images/thumb/d/dc/Crystal_Maiden_Large.png/125px-Crystal_Maiden_Large.png",
+  },
+];
+
 /**
  * @swagger
  * components:
@@ -84,3 +146,10 @@
  *       404:
  *         description: The hero was not found
  */
+
+docRouter.get("/", (req, res, next) => {
+  console.log("work");
+  res.send(data);
+});
+
+export default docRouter;
